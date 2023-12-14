4138d769a025074b2399af8315d448cd13ed60ba
Gabriel <gszcode@gmail.com>
2023-12-14 00:23:13 -0300

    configuracion de db

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,23 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize('ecommerce', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5434
-})
+const isTestEnvironment = process.env.NODE_ENV === 'test'
+
+export const sequelize = new Sequelize(
+  isTestEnvironment ? 'ecommerce_test' : 'ecommerce',
+  'postgres',
+  'password',
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5434
+  }
+)
+
+export async function syncDatabase() {
+  try {
+    await sequelize.sync({ force: true })
+    console.log('Database synchronized successfully')
+  } catch (error) {
+    console.error(`Unable to synchronize the database: ${error}`)
+  }
+}
