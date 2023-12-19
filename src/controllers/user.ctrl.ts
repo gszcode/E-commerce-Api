017bc6a91b40f8d8590d3aa0ec55fce2849d758b
Gabriel <gszcode@gmail.com>
2023-12-18 22:36:18 -0300

    controlador de data de user

--- src/controllers/user.ctrl.ts
+++ src/controllers/user.ctrl.ts
@@ -1,5 +1,19 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { UserSchema } from '../models/User.schema'
 
-const myAccount = async (req: Request, res: Response) => {}
+const account = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = await UserSchema.findOne({
+      where: { email: req.email },
+      attributes: { exclude: ['id', 'password', 'craetedAt', 'updatedAt'] }
+    })
 
-export { myAccount }
+    if (!user) throw new Error('User does not exist')
+
+    return res.status(200).json({ data: user })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { account }
