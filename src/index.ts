a69e890d99ae924f91ee403482ad6bf81dff67f2
Gabriel <gszcode@gmail.com>
2023-12-23 02:40:22 -0300

    se agregaron credenciales de cookie

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,13 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 // Middlewares
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 app.use(express.json())
-app.use(cors())
 app.use(morgan('dev'))
 app.use(cookieParser())
 
