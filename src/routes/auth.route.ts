bc3500860abf66eac47e4afbc8c7899b43f970da
Gabriel <gszcode@gmail.com>
2023-12-23 02:41:53 -0300

    se agregaron las rutas de logout y verificar token

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { login, register } from '../controllers/auth.ctrl'
+import { login, logout, register, verifyToken } from '../controllers/auth.ctrl'
 import { registerValidation } from '../middlewares/register.validation'
 import { loginValidation } from '../middlewares/login.validation'
 const router = Router()
 
 router.post('/register', registerValidation, register)
 router.post('/login', loginValidation, login)
+router.post('/logout', logout)
+router.get('/verify-token', verifyToken)
 
 export default router
