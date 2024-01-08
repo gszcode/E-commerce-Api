751a72537a6ddc10d083a5875ccff69b339f483f
Gabriel <gszcode@gmail.com>
2024-01-08 08:54:40 -0300

    nuevas rutas para recuperar contraseña

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,26 @@
 import { Router } from 'express'
-import { login, logout, register, verifyToken } from '../controllers/auth.ctrl'
-import { registerValidation } from '../middlewares/register.validation'
-import { loginValidation } from '../middlewares/login.validation'
+import {
+  registerValidation,
+  loginValidation,
+  forgotEmailValidation,
+  newPasswordValidation
+} from '../middlewares/auth.validation'
+import {
+  forgotPassword,
+  login,
+  logout,
+  recoveryPassword,
+  register,
+  verifyToken
+} from '../controllers/auth.ctrl'
+
 const router = Router()
 
 router.post('/register', registerValidation, register)
 router.post('/login', loginValidation, login)
 router.post('/logout', logout)
 router.get('/verify-token', verifyToken)
+router.post('/forgot-password', forgotEmailValidation, forgotPassword)
+router.put('/recovery-password/:token', newPasswordValidation, recoveryPassword)
 
 export default router
